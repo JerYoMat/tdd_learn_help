0a83ceae593fd95f5d7dfaeb9c148bebf4d49c61
JerYoMat <jeremy.emata@gmail.com>
2019-02-01 17:05:32 -0500

    filled in lesson topic model tests

--- test/models/lesson_topic_test.rb
+++ test/models/lesson_topic_test.rb
@@ -1,16 +1,33 @@
 require 'test_helper'
 
 class LessonTopicTest < ActiveSupport::TestCase
-  
+  def setup 
+    @lesson = LessonTopics(:test_lesson_topic_1)
+    @other_lesson = LessonTopics(:test_lesson_topic_2)
+  end 
+
   test 'should be valid' do
+    assert @lesson.valid?, 'everything else will fail if this does not pass.  Check you fixtures and setup'
   end 
 
   test 'should have a name' do 
+     @lesson.name = nil 
+     assert_not @lesson.save, 'lesson should require name'
+     @lesson.name = 'test name'
+     assert @lesson.save, 'lesson should save with name'
   end 
 
   test 'should have a description' do
+    @lesson.description = nil 
+    assert_not @lesson.save, 'lesson should require description'
+    @lesson.description = @other_lesson.description
+    assert @lesson.save, 'lesson should save with description'
   end 
   
   test 'should have a course_order' do 
+    @lesson.course_order = nil 
+    assert_not @lesson.save, 'lesson should require course order'
+    @lesson.course_order = 1
+    assert @lesson.save 
   end 
 end
