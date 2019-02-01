76be3c3a02d384896b6f69c443890f24983b457e
JerYoMat <jeremy.emata@gmail.com>
2019-01-31 19:31:49 -0500

    added migrations based on fixtures

--- db/migrate/20190131233153_create_tips.rb
+++ db/migrate/20190131233153_create_tips.rb
@@ -1,8 +1,13 @@
 class CreateTips < ActiveRecord::Migration[5.2]
   def change
     create_table :tips do |t|
-
+      t.string :name
+      t.text :description 
+      t.string :link 
+      t.belongs_to :lesson_topic
+      t.belongs_to :user 
       t.timestamps
     end
+    add_index :tips, [:user_id, :created_at]
   end
 end
--- db/migrate/20190131233215_create_lesson_topics.rb
+++ db/migrate/20190131233215_create_lesson_topics.rb
@@ -1,7 +1,9 @@
 class CreateLessonTopics < ActiveRecord::Migration[5.2]
   def change
     create_table :lesson_topics do |t|
-
+      t.string :name 
+      t.text :description 
+      t.integer :course_order
       t.timestamps
     end
   end
