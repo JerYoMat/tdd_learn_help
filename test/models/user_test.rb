85c751faa4091db904aa7db951749309979b0911
JerYoMat <jeremy.emata@gmail.com>
2019-02-01 16:31:37 -0500

    filled in user_test.rb tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,21 +10,41 @@ class UserTest < ActiveSupport::TestCase
   test 'should be valid'  do
     assert @user1.valid?  
   end 
+
   test 'user name  is required' do
     @user2.name = '   ' 
     assert_not @user2.save 
   end 
+  
   test 'unique email address is required' do 
+    @user1.save 
+    @user2.email = @user1.email.upcase
+    assert_not @user2.save, 'User2 should not save with email equal to existing user'
   end 
-  test 'email address is plausible' do 
+
+  test 'valid email address format is accepted' do 
     valid_email_addresses = %w[test1@example.com TEST@EXAMPLE.CoM with.period@999.com FOREIGN@Gruen.de]
     valid_email_addresses.each do |single_address|
-      assert @user.valid?, "#{single_address.inspect} should be valid"  #this is a custome error message
+      assert @user.valid?, "#{single_address.inspect} should be valid"  
     end 
   end 
-  test 'password matches password_confirmation and length exceeds 7' do 
+
+  test 'invalid email address is rejected' do 
+    invalid_email_addresses = %w[test.com @gmail.com firstname_last testuser@gmail]
+    invalid_email_addresses.each do |single_address|
+      @user1.email = single_address
+      assert_not @user1.valid?, "#{single_address.inspect} should not be considered a valid email"
+    end 
   end 
+
+  test 'password matches password_confirmation and length exceeds 7' do 
+    @user2.password_confirmation = ''
+    assert_not @user2.valid?, 'password and password confirmation should match'
+  end
+
   test 'has_graduated is present' do 
+    @user2.has_graduated = nil 
+    assert_not @user2.valid?
   end 
 
 
