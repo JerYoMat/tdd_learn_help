4e6cd6b735c102659c7c83d89e36bf006a337092
JerYoMat <jeremy.emata@gmail.com>
2019-02-01 23:36:09 -0500

    added helper methods to app controller and some before_actions on user controller, at 11 controller erros

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  
+    include SessionsHelper 
+  
+  def logged_in_user
+    unless logged_in?
+      store_location 
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end 
+
+  def correct_user 
+    set_user
+    redirect_to root_url unless @user == current_user
+  end 
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:create, :edit, :update, :destroy]
+    before_action :correct_user, only: [:edit, :update, :destroy]
+    
+    
     def new
+        @user = User.new 
     end 
   
     def create 
@@ -19,4 +24,12 @@ class UsersController < ApplicationController
   
     def destroy 
     end 
+
+private 
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                               :password_confirmation, :has_graduated)
+  end
+
 end
