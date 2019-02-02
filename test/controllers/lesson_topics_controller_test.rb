6510c9847c2730ef089f88b29281a2d2d8dd9f21
JerYoMat <jeremy.emata@gmail.com>
2019-02-01 23:17:56 -0500

    added lesson topics controller tests

--- test/controllers/lesson_topics_controller_test.rb
+++ test/controllers/lesson_topics_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class LessonTopicsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup 
+    @lesson_topic = lesson_topics(:test_lesson_topic_1)
+  end 
+  
+  test 'index is available' do 
+    get lesson_topics_path
+    assert_response :success 
+  end 
+
+  test 'show is available' do 
+    get lesson_topic_path(@lesson_topic)
+    assert_response :success
+  end 
 end
