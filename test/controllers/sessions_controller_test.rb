ff3b17a627f4320848ac834c931221ee247c85b4
JerYoMat <jeremy.emata@gmail.com>
2019-02-01 21:58:33 -0500

    added sessions controller tests, updated log_in_as for tests, added new view to sessions

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup 
+    @user = users(:test_user_michael)
+  end 
+  
+  test 'should get new' do 
+    get login_path 
+    assert_response :success
+    assert_select "title", "Log In"
+  end 
+
+  test 'after logging out user goes to root' do 
+    log_in_as(@user)
+    get root_path 
+    delete logout_path 
+    assert_redirected_to root_path, 'users that log out should be redirected to home'
+  end 
+
 end
