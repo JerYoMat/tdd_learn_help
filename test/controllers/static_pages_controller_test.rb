5acde11266201b88f2c79abf60e8c9f458ad5cf9
JerYoMat <jeremy.emata@gmail.com>
2019-02-01 22:11:27 -0500

    added static pages controller tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:test_user_michael)
+  end 
+
+  test 'should get home' do 
+    get root_path 
+    assert_response :success 
+    assert_select "title", "Home"
+    
+  end 
+
+  test 'A logged in user should not see an option to sign up' do 
+    log_in_as(@user)
+    get root_path
+    !assert_select 'button li', "Sign up now!"
+  end 
 end
