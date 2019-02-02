85c20651fbbeca207a00cccd46e8de7a0bf04855
JerYoMat <jeremy.emata@gmail.com>
2019-02-02 02:46:11 -0500

    consolidated helper methods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,35 +1,3 @@
 module SessionsHelper
-    def log_in(user)
-        session[:user_id] = user.id
-      end 
-      
-      def current_user
-        if session[:user_id]
-          @current_user ||= User.find_by(id: session[:user_id])  # Same as @current_user = @current_user || User.find_by(id: session[:user_id])
-        end 
-      end
-  
-      def logged_in?
-         !current_user.nil?
-      end 
-      
-      def log_out
-        session.delete(:user_id)
-        @current_user = nil
-      end
-  
-      def log_in_as(user)
-        session[:user_id] = user.id
-      end
-  
-      #The below two methods are there to enable 'friendly-forwarding'.  If a user is prompted to log in when trying to access a page that requires login, then after providing credentials, the user experience is improved if the user is redirected to the original site they were trying to access.  
-      def redirect_back_or(default)
-        redirect_to(session[:forwarding_url] || default)
-        session.delete(:forwarding_url)
-      end
-  
-      def store_location
-        session[:forwarding_url] = request.original_url if request.get?
-      end
-      
+ 
 end
